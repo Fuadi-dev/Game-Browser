425bcced0d0f6372899fab15677fe992d48500e4
Fuadi-dev <daiyanfu4d1@gmail.com>
2025-04-12 05:25:09 +0700

    versi-4

--- game-backend/app/Http/Controllers/Api/AuthController.php
+++ game-backend/app/Http/Controllers/Api/AuthController.php
@@ -64,94 +64,106 @@ class AuthController extends Controller
         ], 200);
     }
 
-        //login post api
-        public function login(Request $request){
-            $request->validate([
-                'email' => 'required',
-                'password' => 'required',
-            ]);
-
-            //if user not found
-            $user = User::where('email', $request->email)->first();
-            if(!$user || !Hash::check($request->password, $user->password)){
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Password or email is incorrect',
-                ], 401);
-            }
-            $token = $user->createToken('auth_token')->plainTextToken;
-            return response()->json([
-                'status' => 'success',
-                'message' => 'User logged in successfully',
-                'user' => $user,
-                'token' => $token,
-            ], 201);
-        }
-        //register post api
-        public function register(Request $request){
-           try{
-            $request->validate([
-                'name' => 'required',
-                'email' => 'required|email|unique:users',
-                'password' => 'required|min:8',
-                'confirm_password' => 'required|same:password',
-            ]);
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password),
-            ]);
-            $token = $user->createToken('auth_token')->plainTextToken;
-            return response()->json([
-                'status' => 'success',
-                'message' => 'User created successfully',
-                'user' => $user,
-                'token' => $token,
-            ], 201);
-           }catch(ValidationException $e){
+    //login post api
+    public function login(Request $request){
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        
+        //Cari user berdasarkan email
+        $user = User::where('email', $request->email)->first();
+        
+        //jika user tidak ditemukan atau password salah
+        if(!$user || !Hash::check($request->password, $user->password)){
             return response()->json([
                 'status' => 'error',
-                'message' => $e->getMessage(),
-            ], 500);
-            }
-            // if(strlen($request->password) < 8) {
-            //     return response()->json([
-            //         'status' => 'error',
-            //         'message' => 'Password must be at least 8 characters',
-            //     ], 400);
-            // }elseif(User::where('email', $request->email)->first()){
-            //     return response()->json([
-            //         'status' => 'error',
-            //         'message' => 'User already exists',
-            //     ], 400);
-            // }elseif($request->password != $request->confirm_password){
-            //     return response()->json([
-            //         'status' => 'error',
-            //         'message' => 'Password and confirm password do not match',
-            //     ], 400);
-            // }
-
-            // $user = User::create([
-            //     'name' => $request->name,
-            //     'email' => $request->email,
-            //     'password' => Hash::make($request->password),
-            // ]);
-            // $token = $user->createToken('auth_token')->plainTextToken;
-            // return response()->json([
-            //     'status' => 'success',
-            //     'message' => 'User registered successfully and  logged in',
-            //     'token' => $token,
-            //     'user' => $user,
-            // ], 201);
+                'message' => 'Password or email is incorrect',
+            ], 401);
         }
-
-        public function logout(Request $request){
-            $request->user()->currentAccessToken()->delete();
-            Auth::guard('web')->logout();
+        
+        //check status user yang sedang login
+        if($user->status != 'free'){
             return response()->json([
-                'status' => 'success',
-                'message' => 'User logged out successfully',
-                'redirect' => 'http://localhost:3000/#login'
-            ], 200);
+                'status' => 'error',
+                'message' => 'Your account is blocked by administrator',
+            ], 401);
         }
+        
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User logged in successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    //register post api
+    public function register(Request $request){
+       try{
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8',
+            'confirm_password' => 'required|same:password',
+        ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+       }catch(ValidationException $e){
+        return response()->json([
+            'status' => 'error',
+            'message' => $e->getMessage(),
+        ], 500);
+        }
+        // if(strlen($request->password) < 8) {
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'message' => 'Password must be at least 8 characters',
+        //     ], 400);
+        // }elseif(User::where('email', $request->email)->first()){
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'message' => 'User already exists',
+        //     ], 400);
+        // }elseif($request->password != $request->confirm_password){
+        //     return response()->json([
+        //         'status' => 'error',
+        //         'message' => 'Password and confirm password do not match',
+        //     ], 400);
+        // }
+
+        // $user = User::create([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'password' => Hash::make($request->password),
+        // ]);
+        // $token = $user->createToken('auth_token')->plainTextToken;
+        // return response()->json([
+        //     'status' => 'success',
+        //     'message' => 'User registered successfully and  logged in',
+        //     'token' => $token,
+        //     'user' => $user,
+        // ], 201);
+    }
+
+    public function logout(Request $request){
+        $request->user()->currentAccessToken()->delete();
+        Auth::guard('web')->logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User logged out successfully',
+            'redirect' => 'http://localhost:3000/#login'
+        ], 200);
+    }
 }
